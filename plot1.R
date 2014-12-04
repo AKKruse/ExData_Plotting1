341538635ce8559fb834ee47585d6f6cd9a432bf
AKruse <amanda@huskers.unl.edu>
2014-12-04 17:12:43 -0600

    White Bkg

--- plot1.R
+++ plot1.R
@@ -12,11 +12,12 @@ rm("house_data")
 hd2$Global_active_power <- as.numeric(hd2$Global_active_power)
 
 # Plot histogram
-hist(hd2$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)",yaxt='n',xaxt='n',bg="white")
+hist(hd2$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)",yaxt='n',xaxt='n')
 axis(side=1, at=seq(0,6,2))
 axis(side=2, at=seq(0,1200,200))
 
 # Copy to a png file
+# stayed with default "white" background because it makes the plot clearer
 dev.copy(png, file="plot1.png", width=480, height=480, units="px")
 dev.off()
 
